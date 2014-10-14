d7eb684c79ef07f6d6948865d04b8d5ebac864a7
Rob Gersteling <degerst@gmail.com>
2014-10-14 21:48:14 +0200

    hfdst 2

--- AirHockey1/res/raw/simple_vertex_shader.glsl
+++ AirHockey1/res/raw/simple_vertex_shader.glsl
@@ -3,5 +3,4 @@ attribute vec4 a_Position;
 void main() {
  
     gl_Position = a_Position;
-    gl_PointSize = 20.0;
 }
--- AirHockey2/src/com/airhockey/android/AirHockeyRenderer.java
+++ AirHockey2/src/com/airhockey/android/AirHockeyRenderer.java
@@ -12,7 +12,7 @@ import static android.opengl.GLES20.*;
 import android.opengl.GLSurfaceView.Renderer;
 
 public class AirHockeyRenderer implements Renderer {
-
+	
 	private static final int POSITION_COMPONENT_COUNT = 2;
 	
 	private static final int BYTES_PER_FLOAT = 4;
@@ -20,29 +20,31 @@ public class AirHockeyRenderer implements Renderer {
 	private final FloatBuffer vertexData;
 	
 	public AirHockeyRenderer() {
+		
 		float[] tableVerticesWithTriangles = {
 				// Triangle 1
-				0f,  0f,
+				0f, 0f,
 				9f, 14f,
 				0f, 14f,
-				
 				// Triangle 2
-				0f,  0f,
-				9f,  0f,
+				0f, 0f,
+				9f, 0f,
 				9f, 14f,
-				
 				// Line 1
-				0f,  7f,
-				9f,  7f,
-				
+				0f, 7f,
+				9f, 7f,
 				// Mallets
-				4.5f,  2f,
+				4.5f, 2f,
 				4.5f, 12f
-		};
-		vertexData = ByteBuffer.allocateDirect(tableVerticesWithTriangles.length * BYTES_PER_FLOAT).order(ByteOrder.nativeOrder()).asFloatBuffer();
-		vertexData.put(tableVerticesWithTriangles);
-	}
-
+				};
+		
+		vertexData = ByteBuffer
+				.allocateDirect(tableVerticesWithTriangles.length * BYTES_PER_FLOAT)
+				.order(ByteOrder.nativeOrder())
+				.asFloatBuffer();
+				vertexData.put(tableVerticesWithTriangles);
+		}
+	
 	@Override
 	public void onDrawFrame(GL10 glUnused) {
 		glClear(GL_COLOR_BUFFER_BIT);
